6b75c48a8b8e105b075a627e9c4059ac6b0b3b0e
abokado2525 <yuushinhana@gmail.com>
2022-08-25 10:43:52 +0900

    ラケットとボールのz軸方向への移動の実装＆卓球台の当たり判定実装

--- sample.rb
+++ sample.rb
@@ -20,9 +20,10 @@ scene = Mittsu::Scene.new
 # カメラの定義
 camera = Mittsu::PerspectiveCamera.new(75.0, ASPECT, 0.1, 1000.0)
 camera.position.y = - 35.0
-# camera.position.x = 40
+# camera.position.y = - 35.0 - 20
 camera.position.z = 50.0
-
+# camera.position.z = 50.0 - 48
+# camera.position.x = 40
 
 # オブジェクト(球体)の定義
 sphere_radius = 1.0
@@ -64,8 +65,19 @@ box_a = Mittsu::Mesh.new(
 # ラケットBの当たり判定で使うブロック
 box_b = Mittsu::Mesh.new(
   Mittsu::SphereGeometry.new(1, 16, 16),
+  Mittsu::MeshBasicMaterial.new(color: 0X0000FF, wireframe:false)
+)
+
+# 卓球台の当たり判定のブロック
+table_box = Mittsu::Mesh.new(
+  Mittsu::SphereGeometry.new(1, 16, 16),
   Mittsu::MeshBasicMaterial.new(color: 0X00FF00, wireframe:false)
 )
+table_box.position.x = 0
+table_box.position.y = 0
+table_box.position.z = -1000 #!遠くに配置することで当たり判定が卓球台面上すべてにあるように見せかける
+table_distance = (table_box.position.z - 2).abs #判定に使う変数
+
 
 #卓球台
 table = create_table
@@ -75,21 +87,17 @@ tableLeg_left = create_tableLeftLegs
 tableLeg_right = create_tableRightLegs
 
 # シーンにオブジェクトを追加する処理
-scene.add(sphere,raketto_a,raketto_b,box_a,box_b,table,tableLeg_left,tableLeg_right)
-
-
-
-# box_distanceと板の重なり具合を可視化
-
+scene.add(sphere, raketto_a, raketto_b, box_a, box_b, table, tableLeg_left, tableLeg_right, table_box)
 
 # 位置調整
 raketto_x = 40  #ラケットのx座標の絶対値
 box_a.position.x = -1 * (box_distance + raketto_x)  #box_distanceはラケットとブロックの距離なので原点からラケットまでの距離を加算する
-box_b.position.x = 1 * (box_distance + raketto_x)#TODO 当たり判定の位置微調整
+box_b.position.x = 1 * (box_distance + raketto_x)
 
 #ボールの移動方向とスピード
 dx = 1
 dy = 0
+dz = -0.1
 flag = 0  # ボールを動かすかのフラグ
 
 # レンダリングをしてくださいと命令する処理かな？
@@ -104,17 +112,26 @@ renderer.window.run do
   if flag == 0
     sphere.position.x += dx
     sphere.position.y += dy
+    sphere.position.z += dz
   end
   
   #ラケットA
   raketto_a.position.x = -1 * raketto_x
-  if renderer.window.key_down?(GLFW_KEY_S)
+  if renderer.window.key_down?(GLFW_KEY_D)
+    box_a.position.y += 1
+    raketto_a.position.y += 1
+  end
+  if renderer.window.key_down?(GLFW_KEY_A)
     box_a.position.y -= 1
     raketto_a.position.y -= 1
   end
   if renderer.window.key_down?(GLFW_KEY_W)
-    box_a.position.y += 1
-    raketto_a.position.y += 1
+    box_a.position.z += 1
+    raketto_a.position.z += 1
+  end
+  if renderer.window.key_down?(GLFW_KEY_S)
+    box_a.position.z -= 1
+    raketto_a.position.z -= 1
   end
 
   #ボールとラケットA側の当たり判定ボックスとの距離
@@ -123,17 +140,26 @@ renderer.window.run do
   if distance <= box_distance + sphere_radius
     dx = 1
     dy = random_Number
+    dz *= -1
   end
 
   #ラケットB
   raketto_b.position.x = 1 * raketto_x
-  if renderer.window.key_down?(GLFW_KEY_DOWN)
-    box_b.position.y -= 1
-    raketto_b.position.y -= 1
-  end
   if renderer.window.key_down?(GLFW_KEY_UP)
+    box_b.position.z += 1
+    raketto_b.position.z += 1
+  end
+  if renderer.window.key_down?(GLFW_KEY_DOWN)
+    box_b.position.z -= 1
+    raketto_b.position.z -= 1
+  end
+  if renderer.window.key_down?(GLFW_KEY_LEFT)
     box_b.position.y += 1
     raketto_b.position.y += 1
+  end
+  if renderer.window.key_down?(GLFW_KEY_RIGHT)
+    box_b.position.y -= 1
+    raketto_b.position.y -= 1
   end
 
   #ボールとラケットB側の当たり判定ボックスとの距離
@@ -142,6 +168,14 @@ renderer.window.run do
   if distance <= box_distance + sphere_radius
     dx = -1
     dy = -1 * random_Number
+    dz *= -1
+  end
+
+  #ボールと卓球台の距離を求める
+  distance_table_to_boll = sphere.position.distance_to(table_box.position)
+  #当たり判定
+  if distance_table_to_boll <= table_distance
+    dz = 0.1
   end
 
   # ボールがラケットより後ろに行った時の処理
@@ -149,6 +183,8 @@ renderer.window.run do
   if sphere.position.x < -40 or sphere.position.x > 40
     scene.remove(sphere)
     sphere.position.x = 0
+    sphere.position.y = 0
+    sphere.position.z = 0
     scene.add(sphere)
     flag = 1
   end
